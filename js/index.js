e13fb100e489fc8732dccb91f799fbbae0d7816f
olha-k <39318385+olha-k@users.noreply.github.com>
2019-07-08 07:31:10 -0400

    third

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const display = document.querySelector('.calculator .display'); 
+
+document.querySelectorAll ('.digits button')
+    .forEach( digit => digit.addEventListener('click', digitPressed));
+
+function digitPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelectorAll ('.operator')
+    .forEach( operator => operator.addEventListener('click', operatorPressed));
+
+function operatorPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.equal').addEventListener ('click', equalPressed); 
+
+function equalPressed() {
+    display.value = eval(display.value);
+}
+
+document.querySelector('.decimal').addEventListener('click', decimalPressed);
+
+function decimalPressed(dot) {
+    if (display.value.includes('.')) {
+        display.value += '';
+    } else
+     display.value += dot.target.innerText;
+}
+
+document.querySelector('.clear').addEventListener ('click', clearAll); 
+
+function clearAll() {
+    display.value = " ";
+}
